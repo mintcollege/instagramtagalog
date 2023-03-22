ad349476df8c81cdf9312bc1e8bc473dca7e02ac
Erlyn <erlynquimson@gmail.com>
2023-03-22 13:32:02 +0800

    simple python program

--- lyn.py
+++ lyn.py
@@ -1,1 +1,17 @@
-print ('Hellosss')
+import random
+import string
+
+def generate_password(length):
+    """Generate a random password with the specified length."""
+    chars = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(chars) for _ in range(length))
+    return password
+
+def main():
+    """Main function."""
+    length = int(input("Enter password length: "))
+    password = generate_password(length)
+    print("Your password is:", password)
+
+if __name__ == '__main__':
+    main()
